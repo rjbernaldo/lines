4332be73382b45fbf4fff35423df1a04fd2d4fd0
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-08-01 18:05:42 +0800

    Add initial draggin functionality

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -8,6 +8,7 @@ class Drawing extends React.Component {
     this.handleMouseDown = this.handleMouseDown(this);
     this.handleMouseUp = this.handleMouseUp(this);
     this.handleMouseMove = this.handleMouseMove(this);
+    this.handleDrag = this.handleDrag(this);
 
     this.state = {
       drawing: null,
@@ -21,7 +22,8 @@ class Drawing extends React.Component {
       .attr('width', 500)
       .attr('height', 500)
       .on('mousedown', this.handleMouseDown)
-      .on('mouseup', this.handleMouseUp);
+      .on('mouseup', this.handleMouseUp)
+      .on('drag', this.handleDrag);
 
     this.setState({ drawing });
   }
@@ -37,7 +39,8 @@ class Drawing extends React.Component {
         .attr('x2', m[0])
         .attr('y2', m[1])
         .attr('stroke-width', 2)
-        .attr('stroke', 'black');
+        .attr('stroke', 'black')
+        .call(component.handleDrag);
 
       component.setState({ line });
 
@@ -60,6 +63,26 @@ class Drawing extends React.Component {
         .attr('x2', m[0])
         .attr('y2', m[1]);
     };
+  }
+
+  handleDrag(component) {
+    return d3.drag()
+      .on('start', null)
+      .on('drag', function (d) {
+        const dx = d3.event.dx;
+        const dy = d3.event.dy;
+        const current = d3.select(this);
+        const x1 = parseFloat(current.attr('x1')) + dx;
+        const y1 = parseFloat(current.attr('y1')) + dy;
+        const x2 = parseFloat(current.attr('x2')) + dx;
+        const y2 = parseFloat(current.attr('y2')) + dy;
+
+        component.state.line
+          .attr('x1', x1)
+          .attr('y1', y1)
+          .attr('x2', x2)
+          .attr('y2', y2);
+      });
   }
 
   render() {
