b683a0d862159b376f4f2ee69bb8e9367c21bdb7
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-08-01 17:44:34 +0800

    Add initial implementation of lines

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -5,21 +5,67 @@ class Drawing extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleMouseDown = this.handleMouseDown(this);
+    this.handleMouseUp = this.handleMouseUp(this);
+    this.handleMouseMove = this.handleMouseMove(this);
+
     this.state = {
       drawing: null,
+      line: null
     };
   }
 
   componentDidMount() {
-    this.setState({
-      drawing: document.getElementById('drawing');
-    });
+    const drawing = d3.select('#drawing')
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 500)
+      .on('mousedown', this.handleMouseDown)
+      .on('mouseup', this.handleMouseUp);
+
+    this.setState({ drawing });
+  }
+
+  handleMouseDown(component) {
+    return function () {
+      const d = component.state.drawing;
+      const m = d3.mouse(this);
+
+      const line = d.append('line')
+        .attr('x1', m[0])
+        .attr('y1', m[1])
+        .attr('x2', m[0])
+        .attr('y2', m[1])
+        .attr('stroke-width', 2)
+        .attr('stroke', 'black');
+
+      component.setState({ line });
+
+      d.on('mousemove', component.handleMouseMove);
+    };
+  }
+
+  handleMouseUp(component) {
+    return function () {
+      component.state.drawing
+        .on('mousemove', null);
+    };
+  }
+
+  handleMouseMove(component) {
+    return function () {
+      const m = d3.mouse(this);
+
+      component.state.line
+        .attr('x2', m[0])
+        .attr('y2', m[1]);
+    };
   }
 
   render() {
     return (
-      <div id="drawing"></div>
-    )
+      <div id="drawing" style={{ height: '500px', width: '500px' }} />
+    );
   }
 }
 
