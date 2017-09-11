829b50f2678053fa3a32b134dfb5915b449316b2
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-09-11 14:21:45 +0800

    Add # of bends and total length details

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,20 +6,14 @@ import GHeading from 'grommet/components/Heading';
 
 import CLineList from '../containers/CLineList';
 
-const Sidebar = ({ mode, toggleMode }) => {
+const Sidebar = ({ mode, toggleMode, numberOfBends, totalLength }) => {
   const style = { padding: '0px', margin: '0px', marginBottom: '5px', WebkitUserSelect: 'none' };
   return (
     <GBox style={{ width: '300px' }}>
       <GBox pad="medium">
-        <GHeading tag="h3" style={{ WebkitUserSelect: 'none' }}>Instructions</GHeading>
-        <GParagraph size="small" style={style}>Click Canvas/Anchor: Add anchor</GParagraph>
-        <GParagraph size="small" style={style}>Double Click Anchor: End drawing</GParagraph>
-        <GParagraph size="small" style={style}>Click & Drag Anchor: Move anchor</GParagraph>
-        <GParagraph size="small" style={style}>Right Click Anchor: Delete anchor</GParagraph>
-        <GParagraph size="small" style={style}>Right Click Length: Edit length</GParagraph>
-        <GParagraph size="small" style={style}>Click & Drag Line: Move line</GParagraph>
-        <GParagraph size="small" style={style}>Right Click Line: Delete line</GParagraph>
-        <GParagraph size="small" style={style}>Right Click Angle: Edit angle</GParagraph>
+        <GHeading tag="h3" style={{ WebkitUserSelect: 'none' }}>Details</GHeading>
+        <GParagraph size="small" style={style}># of Bends: { numberOfBends }</GParagraph>
+        <GParagraph size="small" style={style}>Total Length: { totalLength }</GParagraph>
       </GBox>
       <CLineList />
     </GBox>
--- src/containers/CSidebar.js
+++ src/containers/CSidebar.js
@@ -3,8 +3,32 @@ import Sidebar from '../components/Sidebar';
 import { setDraw, setSelect } from '../actions/mode';
 
 const mapStateToProps = (state) => {
+  const points = state.drawing.points;
+
   return {
     mode: state.mode,
+    numberOfBends: (() => {
+      return Object
+        .keys(points)
+        .map((p) => {
+          const point = points[p];
+          return point.connections.length === 2 ? 1 : 0;
+        });
+    })().reduce((a, b) => a + b, 0),
+    totalLength: generateLines(points).map((line, i) => {
+      const currentId = line[0];
+      const nextId = line[1];
+
+      if (nextId) {
+        const c = points[currentId];
+        const n = points[nextId];
+        const length = calculateLength(c, n);
+
+        return length;
+      } else {
+        return 0;
+      }
+    }).reduce((a, b) => a + b, 0),
   };
 };
 
@@ -14,3 +38,35 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+
+function generateLines(points) {
+  const lines = [];
+  const parsedConnections = [];
+  const pointObjects = Object
+    .keys(points)
+    .map(id => points[id]);
+
+  pointObjects.forEach((point) => {
+    const line = [point.id];
+    point.connections.forEach((connection) => {
+      const connectedPoints = `${line}${connection}`
+        .split('')
+        .sort()
+        .join('');
+
+      if (parsedConnections.indexOf(connectedPoints) === -1) {
+        lines.push(line.concat(connection));
+        parsedConnections.push(connectedPoints);
+      }
+    });
+  });
+
+  return lines;
+}
+
+function calculateLength(c, n) {
+  const a = c.x - n.x;
+  const b = c.y - n.y;
+
+  return parseInt(Math.sqrt(a*a + b*b));
+}
